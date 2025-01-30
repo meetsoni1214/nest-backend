f863f6451217a34ec6cc3a3b250afa3e66d07fca
Meet Soni <meetsoni1214@gmail.com>
2025-01-30 13:17:02 +0530

    feat: mock PrismaService

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -1,17 +1,17 @@
-import { Module } from '@nestjs/common';
-import { HelloworldModule } from './helloworld/helloworld.module';
-import { ObjectivesModule } from './objectives/objectives.module';
-import { DatabaseModule } from './database/database.module';
-import { PrismaService } from './prisma/prisma.service';
-import { KeyResultsModule } from './key-results/key-results.module';
+import {Module} from '@nestjs/common';
+import {HelloworldModule} from './helloworld/helloworld.module';
+import {ObjectivesModule} from './objectives/objectives.module';
+import {DatabaseModule} from './database/database.module';
+import {KeyResultsModule} from './key-results/key-results.module';
 
 @Module({
-  imports: [
-    HelloworldModule,
-    ObjectivesModule,
-    DatabaseModule,
-    KeyResultsModule,
-  ],
-  providers: [],
+    imports: [
+        HelloworldModule,
+        ObjectivesModule,
+        DatabaseModule,
+        KeyResultsModule,
+    ],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
--- server-nest/src/key-results/key-results.service.spec.ts
+++ server-nest/src/key-results/key-results.service.spec.ts
@@ -1,41 +1,39 @@
 import {KeyResultsService} from "./key-results.service";
 import {Test, TestingModule} from "@nestjs/testing";
 import {PrismaService} from "../prisma/prisma.service";
+import {mockDeep} from "jest-mock-extended";
 
 describe('KeyResultService', () => {
+    let service: KeyResultsService
+    const prismaService = mockDeep<PrismaService>()
+    beforeEach(async () => {
+
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [KeyResultsService, {
+                provide: PrismaService,
+                useValue: prismaService
+            }]
+        }).compile();
+
+        service = module.get<KeyResultsService>(KeyResultsService);
+    })
     describe('fetchKeyResult()', () => {
         it('should return key result with the given id', async () => {
-            let service: KeyResultsService
-            let prismaService: PrismaService
-
-            const module: TestingModule = await Test.createTestingModule({
-                providers: [KeyResultsService, PrismaService]
-            }).compile();
-
-
-            service = module.get<KeyResultsService>(KeyResultsService);
-            prismaService = module.get<PrismaService>(PrismaService);
-            await prismaService.keyResult.deleteMany()
-            await prismaService.objective.deleteMany()
-            const objectiveResponse = await prismaService.objective.create({
-                data: {
-                    title: "dummy obj 1",
-                }
-            })
-            const keyResultResponse = await prismaService.keyResult.create({
-                data: {
-                    title: "dummy kr",
-                    initial_value: 100,
-                    current_value: 100,
-                    target_value: 101,
-                    metric: "dummy %",
-                    objective_id: objectiveResponse.id
-                }
-            })
-            const response = await service.getOne(keyResultResponse.id);
-            expect(response).toEqual({...keyResultResponse, id: expect.any(Number)})
-
+            // Arrange
+            const keyResult = {
+                id: 4,
+                title: "dummy kr",
+                initial_value: 100,
+                current_value: 100,
+                target_value: 101,
+                metric: "dummy %",
+                objective_id: 1
+            }
+            prismaService.keyResult.findUnique.mockResolvedValue(keyResult)
+            // Act
+            const response = await service.getOne(keyResult.id);
+            // Assert
+            expect(response).toEqual(keyResult)
         });
-
     });
 });
